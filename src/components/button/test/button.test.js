0335813a9c1f556736cc9ea1e6edc7b93db091de
songx198 <moko1125986@gmail.com>
2020-09-24 22:42:14 -0400

    ajouter afterEach clean les tests

--- src/components/button/test/button.test.js
+++ src/components/button/test/button.test.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Button from './../button';
 
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect";
+
+afterEach(cleanup);
 
 it("renders without crashing", () => {
     const div = document.createElement("div");
@@ -14,3 +16,4 @@ it("renders button correctly", () => {
     const { getByTestId } = render(<Button label="Click"></Button>)
     expect(getByTestId('button')).toHaveTextContent("Click")
 })
+
